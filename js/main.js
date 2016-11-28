ed18f168f6e4b9bf1b44d6bdf64a94041f592880
vurles <portafolio.salinas@gmail.com>
2016-11-28 14:03:43 -0300

    creando funcion para validar numero en input banderas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 $("#demo").intlTelInput();
+var $numeroMax = $("#demo").val();
+
+function validarNum(){
+
+	if ($numeroMax == null || $numeroMax.length =< 9 || /^[0-9]$/.test($numeroMax)) {
+		
+	}
+};
