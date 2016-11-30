d8d742d4b96085cedf608381dd854f44d26bcc8f
vurles <portafolio.salinas@gmail.com>
2016-11-30 13:28:03 -0300

    cambios de portadas, funcion de numeros lista y adjunte mapa pero falla la funcion aun no se ve

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-$("#demo").intlTelInput();
-var $numeroMax = $("#demo").val();
+$("#banderas").intlTelInput();
 
-function validarNum(){
+$(document).ready(function(){
+	$("#banderas").keyup(function(){
+		this.value = (this.value + '').replace(/[^0-9]/g, '');
+	});
 
-	if ($numeroMax == null || $numeroMax.length =< 9 || /^[0-9]$/.test($numeroMax)) {
-		
-	}
-};
+});
+$(document).ready(function(){
+	$("#next").on("click",function(){
+	var maxNumero = $(".numeros").val();
+	if (maxNumero.length != 9) {
+		alert("Ingrese 9 digitos");
+		return false;
+		}
+	});
+});x
